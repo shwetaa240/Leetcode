1969aab3c424c7873f664cdb30994914a2a9eeea
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2025-03-05 17:32:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (61.42%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long sum=5;
-        long long i=4;
-        if(n==1)
-        return (long long)n;
-        if(n==2)
-        return sum;
-        n-=2;
-         while(n!=0)
-         {
-            i=i+4;
-            sum+=i;
-            n--;
-         }
-         return sum;
+        return (2 * (long long )(n -1)* n) + 1;
     }
 };
