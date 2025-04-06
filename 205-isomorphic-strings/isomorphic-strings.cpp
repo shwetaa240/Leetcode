9b4a702320b9cb621a7419a67522a8b292c1e96a
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2025-04-07 00:36:41 +0530

    Time: 6 ms (10.37%) | Memory: 9.4 MB (55.32%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char> mpp1;
-         unordered_map<char,char> mpp2;
-        int i=0,j=0,n=s.size(),m=s.size();
-        char ch1,ch2;
-        if(n!=m)
-        return false;
-         while(i<n)
-         {
-            if(mpp1.find(s[i])==mpp1.end() && mpp2.find(t[j])==mpp2.end())
-           { mpp1[s[i]]=t[j];
-             mpp2[t[j]]=s[i];
-           }
-           else if(mpp1.find(s[i])==mpp1.end())
-           {
-             mpp1[s[i]]=t[j];
-           }
-           else if(mpp2.find(t[j])==mpp2.end())
-           {
-            mpp2[t[j]]=s[i];
-           }
+        unordered_map<char, char> charMap;
 
-            
-               ch1=mpp1[s[i]];
-               ch2=mpp2[t[j]];
-               if(ch1!=t[j] ||ch2!=s[i])
-                return false;
-             i++;j++;
-         }
-         return true;
+        for (int i = 0; i < s.length(); ++i) {
+            char sc = s[i];
+            char tc = t[i];
+
+            if (charMap.count(sc)) {
+                if (charMap[sc] != tc) {
+                    return false;
+                }
+            } else {
+                for (auto& pair : charMap) {
+                    if (pair.second == tc) {
+                        return false;
+                    }
+                }
+                charMap[sc] = tc;
+            }
+        }
+
+        return true;        
     }
 };
