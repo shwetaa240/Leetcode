d8c32017faf360f6562baa9ca608baf248a61021
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2025-05-06 00:10:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.42%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-
-    bool searchMatrix(vector<vector<int>>& matrix, int t) {
-        int i,m=matrix.size()-1,n=matrix[0].size()-1;
-        for(auto &mx:matrix)
-        { 
-            if(mx[n]<t)
-            continue;
-            else if(mx[n]==t)
-            return true;
-            else
-            { int mid,l=0,u=n;
-             while(l<=u)
-             { mid=(l+u)/2;
-                if(mx[mid]==t)
-                return true;
-                else if(mx[mid]<t)
-                {
-                    l=mid+1;
-                }
-                else
-                u=mid-1;
-             }
-             return false;
-            }
+    bool searchMatrix(vector<vector<int>>& mx, int t) {
+        int  m=mx.size(),n = mx[0].size() - 1, l = 0, u = n;
+        while (l < m && u>=0) {
+            if(mx[l][u]>t)
+            u--;
+            else if (mx[l][u] < t) {
+                l++;
+            } else
+               return true;
         }
         return false;
     }
-};
+
+}
+;
