224ed4c9271734fcd07d9ef2a718da3962c7f22d
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2025-08-06 11:54:05 +0530

    Time: 36 ms (97.70%) | Memory: 18.5 MB (92.24%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mx, int t) {
+            int  m=mx.size()-1,n = mx[0].size() - 1, l = 0, u = n;
+        while (l <= m && u>=0) {
+            if(mx[l][u]>t)
+            u--;
+            else if (mx[l][u] < t) {
+                l++;
+            } else
+               return true;
+        }
+        return false;  
+    }
+};
