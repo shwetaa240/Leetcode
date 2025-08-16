fb56af282929063fb61b92abdf71c97108a6b8b0
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2025-08-17 01:02:25 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (71.54%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,18 +1,14 @@
+// C++
 class Solution {
 public:
-    int maximum69Number (int num) {
-        string n=to_string(num);
-        if(n[0]=='6')
-     {
-        n[0]='9';
-        return stoi(n);
-    }
-        for(int i=1;i<n.size();i++)
-        {
-            if(n[i]<n[i-1] )
-            {n[i]='9';
-            break;}
+    int maximum69Number(int num) {
+        string s = to_string(num);
+        for (char& c : s) {
+            if (c == '6') {
+                c = '9';
+                break;
+            }
         }
-        return stoi(n);
+        return stoi(s);
     }
 };
