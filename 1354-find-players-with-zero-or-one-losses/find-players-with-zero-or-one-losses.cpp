55a275796dccabb10b16e454e77721ff626f217c
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2025-09-22 22:09:05 +0530

    Time: 120 ms (80.82%) | Memory: 169 MB (72.25%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,22 +1,25 @@
-class Solution 
-{
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
 public:
-    vector<vector<int>> findWinners(vector<vector<int>>& matches) 
-    {
-        map<int,int> lost;
-        for (auto m : matches)
-        {
-            if (!lost.count(m[0])) lost[m[0]] = 0;
-            lost[m[1]]++;
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        unordered_map<int,int> lost;
+
+        for (auto &m : matches) {
+            if (!lost.count(m[0])) lost[m[0]] = 0;  // winners get initialized
+            lost[m[1]]++;                           // losers increment
         }
-        
+
         vector<int> zero, ones;
-        for (auto[k,l] : lost)
-        {
-            if (l == 0) zero.push_back(k);
-            if (l == 1) ones.push_back(k);
+        for (auto [w,l] : lost) {
+            if (l == 0) zero.push_back(w);
+            else if (l == 1) ones.push_back(w);
         }
-        
-        return {zero,ones};
+
+        sort(zero.begin(), zero.end());
+        sort(ones.begin(), ones.end());
+
+        return {zero, ones};
     }
 };
