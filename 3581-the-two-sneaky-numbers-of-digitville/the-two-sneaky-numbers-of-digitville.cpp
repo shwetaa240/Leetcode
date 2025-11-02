637b90a0149c23e13c39c82176e53b6cc991a1b5
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2025-11-02 15:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (74.86%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        int n = nums.size() - 2;
-        bitset<100> seen = 0;
-        vector<int> ans;
-        for (int x : nums) {
-            if (seen[x]) ans.push_back(x);
-            else seen[x] = 1;
+        int i,n=nums.size(),g1=0,g2=0;
+        long long xorr=0,rightmost;
+        for(i=0;i<n;i++)
+        {
+            xorr^=nums[i];
+        }  
+        for(i=0;i<n-2;i++)
+        xorr^=i;
+
+        rightmost=xorr&(-xorr);
+          for(i=0;i<n;i++){
+            if(nums[i]& rightmost)
+            g1^=nums[i];
+            else
+            g2^=nums[i];
         }
-        return ans;
+            for(i=0;i<n-2;i++){
+            if(i&rightmost)
+            g1^=i;
+            else
+            g2^=i;
+        }
+       return {g1,g2};
     }
 };
