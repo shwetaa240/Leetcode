46aac9576130b203f014c3ccd2125473796392c6
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2025-11-03 14:02:38 +0530

    Time: 4 ms (29.19%) | Memory: 8.2 MB (43.05%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
-    int reverseBits(int n) {
-        return __builtin_bitreverse32(n);
+uint32_t reverseBits(uint32_t n) {
+    uint32_t ans = 0;
+    for (int i = 0; i < 32; i++) {
+        ans = (ans << 1) | (n & 1);
+        n >>= 1;
+    }
+    return ans;
+
     }
 };
