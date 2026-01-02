6e288ebdab90714aa73007735088097f35b3efa2
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-01-02 21:43:33 +0530

    Time: 435 ms (82.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+SELECT 
+Person.firstName,Person.lastName,
+Address.city,Address.state
+FROM Person
+LEFT JOIN Address 
+ON Person.personId=Address.personId; 
