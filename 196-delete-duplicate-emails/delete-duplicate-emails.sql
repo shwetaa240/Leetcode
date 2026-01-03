2440747a6226dd70224dc8d639660a39228c35ba
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-01-03 14:17:53 +0530

    Time: 610 ms (33.79%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
-DELETE p1 FROM Person p1
-JOIN Person p2 
-ON p1.email = p2.email AND p1.id > p2.id;
+DELETE FROM Person p
+WHERE p.id NOT IN (
+    SELECT MIN(id)
+    FROM Person
+    GROUP BY email
+);
