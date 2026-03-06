30c5370e402bcba4dbd628b4527d0fca5f74783d
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-03-06 19:07:37 +0530

    Time: 8 ms (90.52%) | Memory: 34.8 MB (63.18%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+         int m = grid.size();
+        int n = grid[0].size();
+        
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 0) {
+                    q.push({i, j});
+                    vis[i][j] = 0;
+                }
+                else {
+                    vis[i][j]=-1;
+                }
+            }
+        }
+        
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        
+        while(!q.empty()) {
+            
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            
+            
+            for(int i = 0; i < 4; i++) {
+                
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+                
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && vis[nrow][ncol] == -1) { 
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = vis[r][c]+1;
+                }
+            }
+        }
+        
+        
+        return vis;
+    
+    }
+};
