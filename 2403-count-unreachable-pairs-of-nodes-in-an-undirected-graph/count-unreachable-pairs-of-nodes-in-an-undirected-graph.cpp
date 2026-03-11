6d3cb170378c200224143b5e05e796e5527b783a
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-03-12 00:20:10 +0530

    Time: 44 ms (79.35%) | Memory: 150.9 MB (83.59%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,37 +1,55 @@
 class Solution {
 public:
-    void dfs(vector<int>adj[],int src,vector<bool>&vis,int &counter){
-        if(vis[src]) return;
-        vis[src]=true;
-        counter++;
-        for(auto ele:adj[src]){
-            if(!vis[ele]){
-                dfs(adj,ele,vis,counter);
-            }
-        }
+ vector<int> p;
+        vector<int> rank;
+    int find(vector<int> &p, int x) {
+        if (p[x] == x)
+            return x;
+        else
+            return p[x] = find(p, p[x]);
+    }
+    void Union(int x, int y) {
+        int px = find(p, x);
+        int py = find(p, y);
 
+        if (px == py)
+            return;
+        if (rank[px] > rank[py])
+            p[py] = px;
+        else if (rank[px] < rank[py])
+            p[px] = py;
+        else {
+            p[px] = py;
+            rank[py]++;
+        }
     }
     long long countPairs(int n, vector<vector<int>>& edges) {
-        vector<int>adj[n];
-        for(auto ele:edges){
-            adj[ele[0]].push_back(ele[1]);
-            adj[ele[1]].push_back(ele[0]);
-        }
-        long long res = 0;
-        vector<bool>vis(n,false);
-        vector<int>temp;
-        for(int i = 0;i<n;i++){
-            if(!vis[i]){
-                int counter = 0;
-                dfs(adj,i,vis,counter);
-               temp.push_back(counter);
-            }
-        }
-        int total = 0;
-        for(int i = 0;i<temp.size();i++){
-            res+=(long)((long)temp[i]*(long)(n-total-temp[i]));
-            total+=temp[i];
-        }
-        return res;
+       p.resize(n);
+       rank.resize(n,0);
+
+       for(int i=0;i<n;i++)
+       {
+         p[i]=i;
+       }
+       for(auto &e:edges)
+       {
+         int u=e[0], v=e[1];
+         Union(u,v);
+       }
+
+       unordered_map<int,int> mpp;
+       for(int i=0;i<n;i++)
+       {
+         int pi=find(p,i);
+         mpp[pi]++;
+       }
+       long long rem_nodes=n;
+       long long res=0;
+       for(auto &[v,size]:mpp)
+       {
+         res+=size*(rem_nodes-size);          
+         rem_nodes-=size;
+       }
+       return res;
     }
 };
