7939a8d4cf0021cec339f8039502340f68b4f98f
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-03-19 14:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (78.69%) - LeetSync

---
+++ 1493-frog-position-after-t-seconds/frog-position-after-t-seconds.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    double ans=0;
+    int curr;
+    void dfs(vector<int>ad[],int i,int time,int targ,vector<int>&vis,double prob){
+        vis[i]=1;
+        if(targ==i){
+            if(time==curr)ans=prob;
+            else {
+                if(time<curr)
+                if(i!=1 && ad[i].size()<=1)ans=prob;
+            }
+            return;
+        }
+        for(auto x : ad[i]){
+            double sz=ad[i].size();
+            if(i!=1)sz--;
+            double p=1.0/sz;
+            if(!vis[x])
+             dfs(ad,x,time+1,targ,vis,prob*p);
+        }
+        
+    }
+
+
+
+
+    double frogPosition(int n, vector<vector<int>>& edges, int time, int x) {
+        vector<int>ad[n+1];
+        curr=time;
+
+        for(auto x:edges){
+            ad[x[0]].push_back(x[1]);
+            ad[x[1]].push_back(x[0]);
+        }
+
+        vector<int>vis(n+1,0);
+        dfs(ad,1,0,x,vis,1);
+        if(ad[1].size()==0 && x==1)return 1.0;
+       return ans;
+    }
+};
