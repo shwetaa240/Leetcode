b7169f0875c4963b55f3953e38584e63bb061943
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-05-25 22:11:42 +0530

    Time: 562 ms (12.74%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,4 @@
 /* Write your PL/SQL query statement below */
-Select Distinct p1.email as Email 
-FROM Person p1 
-Join Person p2
-ON p1.id<>p2.id and p1.email=p2.email;
+Select email as Email 
+FROM Person 
+Group by email Having Count(email)>1; 
