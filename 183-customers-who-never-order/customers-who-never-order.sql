6a9fee7927bbe83ef9494c63e820018f504de80a
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-05-26 01:44:08 +0530

    Time: 555 ms (62.33%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,6 +1,5 @@
-/* Write your PL/SQL query statement below */
-SELECT c.name as Customers FROM 
-Customers c
-LEFT JOIN Orders o
-ON c.id=o.customerId
-WHERE o.id IS NULL;
+# Write your MySQL query statement below
+Select c.name AS Customers 
+FROM Customers c
+Left Join Orders o 
+On c.id=o.customerId WHERE o.id is Null;
