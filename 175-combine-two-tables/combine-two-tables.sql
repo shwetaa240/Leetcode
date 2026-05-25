99f43574424ea8aef2bca026fc1b026cbfcf0de8
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-05-26 00:41:14 +0530

    Time: 462 ms (64.59%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,7 +1,8 @@
 /* Write your PL/SQL query statement below */
-SELECT 
-Person.firstName,Person.lastName,
-Address.city,Address.state
-FROM Person
-LEFT JOIN Address 
-ON Person.personId=Address.personId; 
+SELECT p.firstName,
+       p.lastName,
+       a.city,
+       a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
