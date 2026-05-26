9743ad0c3f1c3d446ee892ae477d95a38ef44fa2
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-05-27 02:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (99.54%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        unordered_set<char> st;
-        int cnt=0,l=0,u=0;
-        for(char c:word)
-        {   c=tolower(c); l=0,u=0;
-            for(char ch:word){
-               if(c==ch) l++;
-               if(toupper(c)==ch) u++;
+        int cnt = 0, l = 0, u = 0;
+        for (char c = 'a'; c <= 'z'; c++)
+         {
+            l = 0, u = 0;
+            for (char ch : word) 
+            {
+                if (c == ch)
+                    l++;
+                if (toupper(c) == ch)
+                    u++;
             }
-           // if(st.count(tolower(c)) || st.count(toupper(c)))
-            // cnt++;
-            // else
-            // st.insert(tolower(c));
-        
-        if(l>0 && u>0)
-        st.insert(c);
+            if (l > 0 && u > 0)
+                cnt++;
         }
-    
-        return st.size();
+        return cnt;
     }
-
 };
