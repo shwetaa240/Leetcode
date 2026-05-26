ecb5bd869a30860696700b561477a92acff18d78
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-05-26 22:01:35 +0530

    Time: 785 ms (6.97%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,3 +1,4 @@
-SELECT score,
-       DENSE_RANK() OVER(ORDER BY score DESC) AS 'rank'
-FROM Scores;
+SELECT s1.score,
+       (Select count(Distinct s2.score)
+       From Scores s2 Where s2.score>=s1.score) as 'rank'
+FROM Scores s1 Order BY s1.score DESC;
