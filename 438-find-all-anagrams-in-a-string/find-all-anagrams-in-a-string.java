76defda1513bd1519b8d6beb70af4c29bcb11162
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-05-31 21:22:01 +0530

    Time: 10 ms (81.87%) | Memory: 46.8 MB (70.07%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,24 +1,47 @@
+import java.util.*;
+
 class Solution {
+
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res= new ArrayList<>();
-       
-       int n2=s.length(),n1=p.length();
-        char arr[]=p.toCharArray();
-       Arrays.sort(arr);
-       String sp=new String(arr);
-       for(int i=0;i<=n2-n1;i++)
-       {
-          String sub=s.substring(i,i+n1);
-          char ar[]=sub.toCharArray();
-          Arrays.sort(ar);
-          if(sp.equals(new String(ar)))
-          {
-            res.add(i);
-          }
-       }
-      return res;
 
+        List<Integer> res = new ArrayList<>();
 
+        if(p.length() > s.length())
+            return res;
 
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        // frequency of pattern
+        for(char c : p.toCharArray()) {
+            pCount[c - 'a']++;
+        }
+
+        // first window
+        for(int i=0; i<p.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;
+        }
+
+        // check first window
+        if(Arrays.equals(pCount, sCount)) {
+            res.add(0);
+        }
+
+        // sliding window
+        for(int i=p.length(); i<s.length(); i++) {
+
+            // add new character
+            sCount[s.charAt(i) - 'a']++;
+
+            // remove old character
+            sCount[s.charAt(i - p.length()) - 'a']--;
+
+            // compare frequencies
+            if(Arrays.equals(pCount, sCount)) {
+                res.add(i - p.length() + 1);
+            }
+        }
+
+        return res;
     }
 }
