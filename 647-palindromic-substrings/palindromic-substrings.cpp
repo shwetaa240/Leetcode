ea40eb34b02eed38868b20eb4794195889f5895e
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-06-08 00:47:06 +0530

    Time: 34 ms (25.87%) | Memory: 34.6 MB (14.55%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    int solveMem(string& s, int i, int j, vector<vector<int>>& dp) {
-    if (i >= j) return 1;           
-    if (dp[i][j] != -1) return dp[i][j]; 
-    if (s[i] != s[j]) return dp[i][j] = 0; 
-    return dp[i][j] = solveMem(s, i + 1, j - 1, dp); 
-}
 
-int countSubstrings(string s) {
-    int n = s.size(), cnt = 0;
-    vector<vector<int>> dp(n, vector<int>(n, -1));
-    for (int i = 0; i < n; i++)
-        for (int j = i; j < n; j++)
-            cnt += solveMem(s, i, j, dp);
-    return cnt;
-}
+    int countSubstrings(string s) {
+       
+       int l=1,i,j=0,c=0,n=s.size();
+vector<vector<int>>dp (n,vector<int>(n,0));
+       for(l=1;l<=n;l++)
+       {
+        for(i=0;i+l-1<n;i++)
+          {
+            j=i+l-1;
+            if(i==j) dp[i][j]=1;
+            else if(i+1==j)
+            {
+              dp[i][j]=(s[i]==s[j]);
+            }
+            else
+            dp[i][j]= ((s[i]==s[j] )&& dp[i+1][j-1]);
+            
+            if(dp[i][j])
+           c++;   
+        
+          } 
+        }
+       return c;
+    }
 };
+
