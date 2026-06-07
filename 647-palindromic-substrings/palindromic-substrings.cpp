5c21be428bfd17a04b95af0a44bbae681b28dcb5
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-06-07 23:51:26 +0530

    Time: 39 ms (23.92%) | Memory: 34.6 MB (14.55%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-int palin(string s,int i,int j)
-{
-   while(i<j)
-   {
-    if(s[i]!=s[j])
-    return false;
-    i++;
-    j--;
-   }
-  return true;
+    int solveMem(string& s, int i, int j, vector<vector<int>>& dp) {
+    if (i >= j) return 1;           
+    if (dp[i][j] != -1) return dp[i][j]; 
+    if (s[i] != s[j]) return dp[i][j] = 0; 
+    return dp[i][j] = solveMem(s, i + 1, j - 1, dp); 
 }
-    int countSubstrings(string s) {
-       int i=0,j=0,c=0,n=s.size();
-       for(i=0;i<n;i++)
-       {
-        for(j=i;j<n;j++)
-          {
-            if(palin(s,i,j))
-            {
-              c++;
-            }
-          } 
-        
-        }
-       return c;
-    }
-};
 
+int countSubstrings(string s) {
+    int n = s.size(), cnt = 0;
+    vector<vector<int>> dp(n, vector<int>(n, -1));
+    for (int i = 0; i < n; i++)
+        for (int j = i; j < n; j++)
+            cnt += solveMem(s, i, j, dp);
+    return cnt;
+}
+};
