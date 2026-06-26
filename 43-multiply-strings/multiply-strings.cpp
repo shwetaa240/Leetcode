9ffce388401a5717210478ff5f881a010891a496
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-06-27 02:37:46 +0530

    Time: 3 ms (57.84%) | Memory: 9.4 MB (93.81%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int n1=num1.size();
+        int n2=num2.size();
+
+        if(num1=="0"||num2=="0")
+            return "0";
+
+        vector<int> num(n1+n2,0);
+
+        for(int i=n1-1;i>=0;i--){
+            for(int j=n2-1;j>=0;j--){
+
+                num[i+j+1]+=(num1[i]-'0')*(num2[j]-'0');
+
+                num[i+j]+=num[i+j+1]/10;
+
+                num[i+j+1]%=10;
+            }
+        }
+
+        int i=0;
+
+        while(i<num.size() && num[i]==0)
+            i++;
+
+        string res;
+
+        while(i<num.size())
+            res.push_back(num[i++]+'0');
+
+        return res;
+    }
+};
