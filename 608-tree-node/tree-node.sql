2b44246a1ad0a70c65f4ab56cae93fdb80534294
Shweta Srivastava <2k23csaiml2310902@gmail.com>
2026-09-16 00:23:33 +0530

    Time: 454 ms (94.41%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -2,7 +2,7 @@
 Select id, 
 Case
    When p_id is NULL Then 'Root'
-   When id Not in (Select Distinct p_id From Tree Where p_id is NOT null) Then 'Leaf' 
-   Else 'Inner'
+   When id in (Select Distinct p_id From Tree Where p_id) Then 'Inner'
+   Else 'Leaf' 
 End as type
 From Tree;
